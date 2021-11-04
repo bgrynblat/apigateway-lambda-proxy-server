0ec2c8a363e721f94b5ca36d75dfde571bbeac60
Ben Grynblat <benjamin.grynblat@gmail.com>
2021-11-04 17:17:03 +0000

    handle sigterm

--- index.js
+++ index.js
@@ -57,6 +57,33 @@ app.all('*', (req, res) => {
   })
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port} lambda:${lambdaProxy}`)
 })
+
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+let connections = [];
+
+server.on('connection', connection => {
+    connections.push(connection);
+    connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
+
+function shutDown() {
+    console.log('Received kill signal, shutting down gracefully');
+    server.close(() => {
+        console.log('Closed out remaining connections');
+        process.exit(0);
+    });
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+
+    connections.forEach(curr => curr.end());
+    setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
